f093b9565c525e6dd998d2d64dd63fe365d51b02
Aierto Francisco Lucero <aiertofranciscolucero@Aiertos-MacBook-Pro.local>
2021-04-25 11:41:37 +0800

    Added Phone Controllers

---
+++ app/Http/Controllers/PhonesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Phones;
+
+class PhonesController extends Controller
+{
+    public function getPhoneList(){
+        return Phones::all();
+    }
+
+    public function uploadPhone(Request $request){
+        
+        $validated = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'image_url' => 'required',
+        ]);
+        
+        $phone = new Phones;
+
+        $phone->name = $request->name;
+        $phone->description = $request->description;
+        $phone->image_url = $request->image_url;
+
+        if(!$phone->save()){
+            return response()->json([
+                'success' => false
+            ]);
+        } else {
+            return response()->json([
+                'success' => true
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/phones', 'PhonesControlller@getPhoneList');
+Route::post('/phones', 'PhonesControlller@uploadPhone');
